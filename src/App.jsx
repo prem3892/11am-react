35e909061032b88859204f6f5f475f5587e2b27c
prem singh <premrathore654@gmail.com>
2025-04-02 11:43:07 +0530

    sdvs

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,23 @@ import Login from "./pages/Login";
 import Protect from "./components/auth/Protect";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" Component={HeroSection} />
-        <Route path="/dashboard" element={<Protect />} />
+  return (<>
+  
+  <Login />
+  </>
+    // <BrowserRouter>
+    //   <Header />
+    //   <Routes>
+    //     <Route path="/" Component={HeroSection} />
+    //     <Route path="/dashboard" element={<Protect />} />
 
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
   );
 }
 export default App;
